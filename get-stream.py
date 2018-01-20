ae9fc44dd29651effc992e0abeca686ce88ddaca
Arthit Suriyawongkul <arthit@gmail.com>
2018-01-20 16:05:10 +0700

    Read configuration data from a separated file

--- get-stream.py
+++ get-stream.py
@@ -1,19 +1,50 @@
-from tweepy import OAuthHandler
-from tweepy import Stream
-from tweepy.streaming import StreamListener
+import configparser
 import argparse
 from collections import OrderedDict
 
-parser = argparse.ArgumentParser(
-    description='Get Twitter stream in a language specified. Filter by common word list.')
-parser.add_argument('lang_code')
-parser.add_argument('output_json_file')
-args = vars(parser.parse_args())
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+CONFIG_FILENAME = 'accounts.ini'
+
+def read_config(config_filename):
+    config = configparser.ConfigParser()
+
+    if not config.read(config_filename):
+        print('Cannot read configuration file: {}'.format(config_filename))
+        return None
+
+    if 'twitter' not in config:
+        print('Cannot find [Twitter] section in: {}'.format(config_filename))
+        return None
+    twitter_config = config['twitter']
+
+    if 'CONSUMER_KEY' not in twitter_config:
+        print('CONSUMER_KEY not found.')
+        return None
+
+    if 'CONSUMER_SECRET' not in twitter_config:
+        print('CONSUMER_SECRET not found.')
+        return None
+
+    if 'ACCESS_TOKEN' not in twitter_config:
+        print('ACCESS_TOKEN not found.')
+        return None
+
+    if 'ACCESS_SECRET' not in twitter_config:
+        print('ACCESS_SECRET not found.')
+        return None
+
+    return config
 
 class MyListener(StreamListener):
+    def __init__(self, filename):
+        self.output_filename = filename
+
     def on_data(self, data):
         try:
-            with open(args['output_json_file'], 'a') as f:
+            with open(self.output_filename, 'a') as f:
                 f.write(data)
                 return True
         except BaseException as e:
@@ -24,26 +55,40 @@ class MyListener(StreamListener):
         print(status)
         return True
 
-words = []
-common_words_filename = 'words-' + args['lang_code'] + '.txt'
-print("Language: {}".format(args['lang_code']))
-print("Use word list from: {}".format(common_words_filename))
+def main():
+    parser = argparse.ArgumentParser(
+        description='Get Twitter stream in a language specified. Filter by common word list.')
+    parser.add_argument('lang_code', help='ISO language code (will use to pick word list)')
+    parser.add_argument('output_json_file', help='Output file, will be written in JSON format')
+    args = vars(parser.parse_args())
 
-with open(common_words_filename) as f:
-    words = f.readlines()
-_words = [w.split() for w in words]
-_words = [w for x in _words for w in x]
-words = list(OrderedDict((w, True) for w in _words).keys())
-del words[400:]
-print("Number of words: {}".format(len(words)))
+    lang = args['lang_code']
+    output_filename = args['output_json_file']
 
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
-ACCESS_TOKEN = ''
-ACCESS_SECRET = ''
+    words = []
+    common_words_filename = 'words-' + lang + '.txt'
+    with open(common_words_filename) as f:
+        words = f.readlines()
+    _words = [w.split() for w in words] # break compound words into single words
+    _words = [w for x in _words for w in x] # flattening the list
+    words = list(OrderedDict((w, True) for w in _words).keys()) # remove duplicates, preserving order
+    del words[400:] # cut it to 400 words (the limit by Twitter API)
 
-auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+    print("Languages: {}".format(lang))
+    print("Use word list from: {}".format(common_words_filename))
+    print("Number of single words: {}".format(len(words)))
 
-twitter_stream = Stream(auth, MyListener())
-twitter_stream.filter(track=words, languages=[args['lang_code']])
+    config = read_config(CONFIG_FILENAME)
+    if not config:
+        print("Please check values in configuration file: {}".format(CONFIG_FILENAME))
+        return
+    twitter_config = config['twitter']
+
+    print("Connecting to Twitter stream...")
+    auth = OAuthHandler(twitter_config['CONSUMER_KEY'], twitter_config['CONSUMER_SECRET'])
+    auth.set_access_token(twitter_config['ACCESS_TOKEN'], twitter_config['ACCESS_SECRET'])
+
+    twitter_stream = Stream(auth, MyListener(output_filename))
+    twitter_stream.filter(track=words, languages=[lang])
+
+if __name__ == "__main__": main()
